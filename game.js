8ad0974e0e262b86574560723f4b8bad6cb16f4d
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-05-20 17:38:21 +0545

    used prettier to make it look better

--- game.js
+++ game.js
@@ -1,20 +1,22 @@
 import inquirer from "inquirer";
 
-
-async function askGuess(){
-    const {guess}= await inquirer.prompt([{
-        type:"input",
-        name:"guess",
-        message:"Enter your guess :",
-        validate:(value)=>{
-            const num =Number(value)
-            if(isNaN(num)) return "Please enter a valid number"
-            if(num < 1 || num > 100) return "Please enter a number between 1 and 100"
-            return true
-        }
-    }])
-    return Number(guess)
-} 
+async function askGuess() {
+  const { guess } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "guess",
+      message: "Enter your guess :",
+      validate: (value) => {
+        const num = Number(value);
+        if (isNaN(num)) return "Please enter a valid number";
+        if (num < 1 || num > 100)
+          return "Please enter a number between 1 and 100";
+        return true;
+      },
+    },
+  ]);
+  return Number(guess);
+}
 
 export async function easy() {
   console.log("you have choose easy mode you have 10 lives");
@@ -22,25 +24,24 @@ export async function easy() {
   console.log("Computer has guessed the Number between 1 - 100");
   let lives = 10;
   let attempts = 0;
-  while(lives>0){
+  while (lives > 0) {
     const ans = await askGuess();
-    if(ans===guess){
-        console.log(`you win!!!. You guessed the number in ${attempts} attempts`);
-        break
-    }else if(ans<guess){
-        console.log("Go higher ");
-       
-    }else{
-        console.log("Go lower");
+    if (ans === guess) {
+      console.log(`you win!!!. You guessed the number in ${attempts} attempts`);
+      break;
+    } else if (ans < guess) {
+      console.log("Go higher ");
+    } else {
+      console.log("Go lower");
     }
     lives--;
     attempts++;
     console.log(`You have ${lives} lives left`);
   }
-  if(lives===0){
+  if (lives === 0) {
     console.log("you Loose T_T !!! ");
   }
-  await restart()
+  await restart();
 }
 
 export async function medium() {
@@ -50,25 +51,24 @@ export async function medium() {
   let lives = 5;
   let attempts = 0;
 
- while(lives>0){
+  while (lives > 0) {
     const ans = await askGuess();
-    if(ans===guess){
-        console.log(`you win!!!. You guessed the number in ${attempts} attempts`);
-        break
-    }else if(ans<guess){
-        console.log("Go higher ");
-       
-    }else{
-        console.log("Go lower");
+    if (ans === guess) {
+      console.log(`you win!!!. You guessed the number in ${attempts} attempts`);
+      break;
+    } else if (ans < guess) {
+      console.log("Go higher ");
+    } else {
+      console.log("Go lower");
     }
     lives--;
     attempts++;
     console.log(`You have ${lives} lives left`);
   }
-  if(lives===0){
+  if (lives === 0) {
     console.log("you Loose T_T !!! ");
   }
-   await restart()
+  await restart();
 }
 
 export async function hard() {
@@ -77,55 +77,56 @@ export async function hard() {
   console.log("Computer has guessed the Number between 1 - 100");
   let lives = 3;
   let attempts = 0;
-  while(lives>0){
+  while (lives > 0) {
     const ans = await askGuess();
-    if(ans===guess){
-        console.log(`you win!!!. You guessed the number in ${attempts} attempts`);
-        break
-    }else if(ans<guess){
-        console.log("Go higher ");
-       
-    }else{
-        console.log("Go lower");
+    if (ans === guess) {
+      console.log(`you win!!!. You guessed the number in ${attempts} attempts`);
+      break;
+    } else if (ans < guess) {
+      console.log("Go higher ");
+    } else {
+      console.log("Go lower");
     }
     lives--;
     attempts++;
     console.log(`You have ${lives} lives left`);
   }
-  if(lives===0){
+  if (lives === 0) {
     console.log("you Loose T_T !!! ");
   }
-  await restart()
+  await restart();
 }
 
 export async function start() {
-  const {level}=await inquirer.prompt([{
-    type:"list",
-    name:"level",
-    message:"Choose your difficulty level",
-    choices:[
-      {name:"Easy",value:"easy"},
-      {name:"Medium",value:"medium"},
-      {name:"Hard",value:"hard"}
-    ]
-  }])
+  const { level } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "level",
+      message: "Choose your difficulty level",
+      choices: [
+        { name: "Easy", value: "easy" },
+        { name: "Medium", value: "medium" },
+        { name: "Hard", value: "hard" },
+      ],
+    },
+  ]);
   if (level === "easy") {
-   await  easy();
+    await easy();
   } else if (level === "medium") {
-   await medium();
-  } else  {
+    await medium();
+  } else {
     await hard();
-  } 
-  
+  }
 }
 
-async function  restart() {
- const {ans} = await inquirer.prompt([{
-    type:"confirm",
-    name:"ans",
-    message:"Do you want to play again?",
-    default:false
- }])
- return ans ? start() : console.log("Thank you for playing! Goodbye!");
+async function restart() {
+  const { ans } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "ans",
+      message: "Do you want to play again?",
+      default: false,
+    },
+  ]);
+  return ans ? start() : console.log("Thank you for playing! Goodbye!");
 }
-
